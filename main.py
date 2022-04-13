cfc4ddc29864f59021abe3b6cf1fed16541c8ff6
Dominyk4s <62887642+Dominyk4s@users.noreply.github.com>
2022-04-13 03:10:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+import whatsapp_chrome_handler as wa
+import parameters as param
+import pandas as pd
+import time
+
+
+def generate_messages(row, text_message_form):
+    text_messages = []
+    try:
+        for message in text_message_form:
+            text_messages.append(message.format(**row))
+    except:
+        print('LT: Patikrink, ar duomenų faile yra visi tekste nurodyti stulpeliai, kuriuos nori naudoti žinutėje')
+        print('EN: Check if there are no missing columns from text input string in data file')
+        # sys.exit(1)
+    return text_messages
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Reading Data
+    df = pd.read_csv(param.FILE_PATH)
+    df.drop_duplicates(subset=[param.PHONE_COLUMN], inplace=True)  # Removing duplicates if phone number repeats
+    if param.SELECT_RANDOM:
+        df = df.sample(n=param.RANDOM_COUNT)
+    df = df.fillna(value='')  # Filling na not to show "None" in text messages (shows '' instead)
+
+    # Overwrite phone number if you want to test in on your phone number or any other single phone number:
+    # df[param.PHONE_COLUMN]=37060000000
+
+    # Generating message text as pandas column
+    messages = []
+    df['message_to_send'] = ''
+    for index, row in df.iterrows():
+        message = generate_messages(row=row, text_message_form=param.text_message_form)
+        df.at[index, 'message_to_send'] = message
+
+    # Getting driver (opening browser)
+    driver = wa.get_driver()
+
+    # Logging in to WhatsApp
+    wa.login(driver)
+    print('LT: Nuskenuokite QR kodą, prisijungus spauskinte "Enter"')
+    print('EN: Login by scanning QR Code, and press Enter')
+    input()
+    print('Prisijungta prie WhatsApp/Logged in to WhatsApp')
+    time.sleep(3)
+
+    # Sending messages to all people from the file
+    for index, row in df.iterrows():
+        wa.get_contact_compose_and_send(phone=df.at[index, param.PHONE_COLUMN], driver=driver,
+                                        text_messages=df.at[index, 'message_to_send'])
+        print('Išsiųstos visos žinutės kontaktui/Finished with contact')
+
+    # Exiting Chrome:
+    driver.quit()
---
+++ whatsapp_chrome_handler.py
@@ -0,0 +1,48 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+
+def login(driver):
+    driver.get("https://web.whatsapp.com")
+
+
+def get_contact(phone, driver):
+    driver.get("https://web.whatsapp.com/send?phone=‭" + str(phone))
+    print('Loaded contact: {}'.format(phone))
+
+
+def find_text_input(driver, inp_xpath='//div[@title="Type a message"]'):
+    input_box = driver.find_element(by=By.XPATH, value=inp_xpath)
+    print('FOUND INPUT ELEMENT')
+    # print(input_box)
+    return input_box
+
+
+def enter_message(text, input_box):
+    input_box.send_keys(text)  # + Keys.ENTER)
+    print('ENTERED MESSAGE')
+
+
+def send_message(input_box):
+    input_box.send_keys(Keys.ENTER)
+    print('Sent message')
+
+
+def get_contact_compose_and_send(driver, text_messages, phone, inp_xpath='//div[@title="Type a message"]'):
+    get_contact(phone, driver)
+    time.sleep(8)
+    input_box = find_text_input(driver, inp_xpath=inp_xpath)
+    time.sleep(2)
+    for text in text_messages:
+        enter_message(text=text, input_box=input_box)
+        time.sleep(1)
+        send_message(input_box)
+        time.sleep(2)
+
+
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()))
